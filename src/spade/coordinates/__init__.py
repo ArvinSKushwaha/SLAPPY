2eca519ad093f0bf90310e1c00d1137dede1d789
Arvin Singh Kushwaha <askushwa@ncsu.edu>
2021-10-06 10:03:52 -0400

    Initialize subpackage for handling curvilinear coordinate systems.

---
+++ src/spade/coordinates/__init__.py
@@ -0,0 +1,4 @@
+"""The coordinates package contains classes that describe the curvilinear coordinate systems in which we solve
+these partial differential equation systems. This provides functionality to support non-euclidean geometries as well as
+non-cartesian coordinate systems. This functionality is given by the Coordinate class, which constructs a TensorField
+defining the geometry of the space in which the systems resides."""
